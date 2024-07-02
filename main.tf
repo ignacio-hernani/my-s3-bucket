9cee5b784875732898389ab78f2284d8f971003c
ignacio-hernani <89387442+ignacio-hernani@users.noreply.github.com>
2024-07-02 16:02:57 +0200

    Create main.tf with provider and module

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.56.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+module "s3-bucket_example_complete" {
+  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
+  version = "4.1.2"
+}
