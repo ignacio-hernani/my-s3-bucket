0f627a14e9d38704af93530b52864bcae94f8c50
ignacio-hernani <h.ignacio@pm.me>
2025-03-28 13:37:37 +0100

    Update tf version and module

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.56.1"
+      version = "5.93.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "eu-west-2"
+  region = "eu-west-1"
 }
 
 module "s3-bucket_example_complete" {
-  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
-  version = "4.1.1"
+  source  = "terraform-aws-modules/terraform-aws-s3-bucket/examples/complete"
+  version = "4.6.0"
 }
