c7423de2a90693bc1fc38cb0094c72631a44c816
ignacio-hernani <h.ignacio@pm.me>
2025-03-28 13:44:42 +0100

    Update module path

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ provider "aws" {
 }
 
 module "s3-bucket_example_complete" {
-  source  = "app.terraform.io/ignacio-org/s3-bucket/aws/examples/complete"
+  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
   version = "4.1.2"
 }
